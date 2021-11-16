7b6dfac647202cda9d272fcfee05f86d27ea03ee
tjuantong <77320112+tjuantong@users.noreply.github.com>
2021-11-16 17:57:52 +0800

    add webivent_draft files

---
+++ webivent/webivent_draft/urls.py
@@ -0,0 +1,35 @@
+"""webivent_draft URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+#added imports 
+from draftwebivent.views import *
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('admin/', admin.site.urls),
+    path('typeUser/', typeUser, name="typeUser"),
+    path('web/', web, name="web"),
+    path('loginPage/', loginPage, name="loginPage"),
+    path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
+    path('userdashboard/', userdashboard, name="dashboard"),
+    path('myProfile/', myProfile, name="myProfile"),
+    path('editProfile/', editProfile, name="editProfile"),
+    path('logout/', editProfile, name="logout"),
+
+]
